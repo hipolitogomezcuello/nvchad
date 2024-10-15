2938dfece8b42da5766cb2fa5e9bda1448f0bfd7
Hipolito Gomez Cuello <hipolitogomezcuello@gmail.com>
2024-10-15 12:25:11 -0300

    add clangd for cpp

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Requires to have these servers:
 ```
 brew install lua-language-server
 go install golang.org/x/tools/gopls@latest
+brew install llvm
 ```
 
 Uses Mason for LSPs configurations, run `:MasonInstallAll` while in neovim
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 -- EXAMPLE
 -- local servers = { "html", "cssls", "gopls" }
-local servers = {}
+local servers = { "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
